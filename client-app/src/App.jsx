2c6426e2d1ef31354a2e97308ab0953b41a24203
Bright Jonathan <brightjonathan000@gmail.com>
2024-01-22 23:21:26 +0100

    installing the dependencies

--- client-app/src/App.jsx
+++ client-app/src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
 
 
 const App = () => {
@@ -10,6 +10,6 @@ const App = () => {
     </h1>
     </div>
   )
-}
+};
 
 export default App;
