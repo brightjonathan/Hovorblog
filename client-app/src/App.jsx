96ec00b32421fbc3c398da3f6ca24990bb07c401
Bright Jonathan <brightjonathan000@gmail.com>
2024-01-23 10:05:53 +0100

    working on the header navbar

--- client-app/src/App.jsx
+++ client-app/src/App.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import './App.css';
 import {  Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import HeaderNarbar from './Component/HeaderNarbar';
+
 
 const App = () => {
   return (
     <div>
+      <HeaderNarbar />
       <Routes>
       <Route path='/' element={<Home />} />
       </Routes>
