1b14f377bb6ed2001c7b7cd529fcd936b6938816
Bright Jonathan <brightjonathan000@gmail.com>
2024-01-26 12:49:58 +0100

    working on the dashbaord profile page

---
+++ client-app/src/Component/Card/Card.jsx
@@ -0,0 +1,11 @@
+import styles from './Card.module.scss';
+
+const Card = ({ children, cardClass }) => {
+  return (
+    <div>
+      <div className={`${styles.card} ${cardClass}`}>{children}</div>;
+    </div>
+  )
+}
+
+export default Card;
