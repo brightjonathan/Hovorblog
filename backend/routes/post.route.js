dd4dcdeb3b00453a17dcdcd80d501d090bdf0869
Bright Jonathan <brightjonathan000@gmail.com>
2024-02-17 15:15:34 +0100

    created the api for getting all post

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { verifyToken } from '../middleware/verify.user.js';
-import { createpost} from '../controller/post.controller.js';
+import { createpost, getPosts } from '../controller/post.controller.js';
 
 const postrouter = express.Router();
 
 postrouter.post('/create', verifyToken, createpost);
+postrouter.get('/getposts', getPosts);
 
 export default postrouter;
 
